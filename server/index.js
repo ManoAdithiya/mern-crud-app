e4324f3907a30264b1e7660f33c57b7db65f6838
Mano Adithiya V <manoadithiya2002e@gmail.com>
2025-12-26 11:47:18 +0530

    Initial MERN CURD Project

---
+++ server/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const User = require("./models/Users");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/curd")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  User.find({})
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+app.get("/getUser/:id", (req, res) => {
+  const id = req.params.id;
+  User.findById({ _id: id })
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+app.delete('/deleteUser/:id',(req,res)=>{
+  const id=req.params.id
+  User.findByIdAndDelete({_id:id})
+  .then(res=>res.json(res))
+  .catch(err=>res.json(err))
+})
+
+app.put("/updateUser/:id", (req, res) => {
+  const id = req.params.id;
+  User.findByIdAndUpdate(
+    { _id: id },
+    { name: req.body.name, email: req.body.email, age: req.body.age }
+  )
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+app.post("/createUser", async (req, res) => {
+  try {
+    console.log(req.body); // 👈 DEBUG LINE
+
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    console.error("CreateUser Error:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server is Running on port 3001.");
+});
