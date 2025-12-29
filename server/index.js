fda30d027af8928fe424e4a7c451b033f3bcef40
Mano Adithiya V <manoadithiya2002e@gmail.com>
2025-12-29 18:49:20 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://localhost:27017/curd")
+  .connect("mongodb+srv://manoadithiya2002e_db_user:mano1005@cluster0.tzpprzl.mongodb.net/?appName=Cluster0")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
